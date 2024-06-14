d77a075964395df944894e723d5710911ca78ed3
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-06-14 22:04:47 +0800

    login

--- public/js/login.js
+++ public/js/login.js
@@ -2,11 +2,11 @@
 //     e.preventDefault();
 //     $("#loginModal").show();
 // });
-$(document).ready(function(){
-    $("#loginbtnSubmit").on('click', function(e) {
+$(document).ready(function () {
+    $("#loginbtnSubmit").on("click", function (e) {
         e.preventDefault();
 
-        var data = $('#loginForm')[0];
+        var data = $("#loginForm")[0];
         var formData = new FormData(data);
         console.log(data);
 
@@ -16,50 +16,51 @@ $(document).ready(function(){
             data: formData,
             contentType: false,
             processData: false,
-            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'), },
+            headers: {
+                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+            },
             dataType: "json",
             accepts: {
-                json: 'application/json'
+                json: "application/json",
             },
-            success: function(data) {
+            success: function (data) {
                 console.log(data);
                 // table.row(cRow).data(data).invalidate().draw(false);
 
                 if (data.errors) {
                     bootbox.alert({
                         message: data.errors,
-                        className: 'rubberBand animated'});
+                        className: "rubberBand animated",
+                    });
                     // setTimeout(function(){
                     //     window.location.href = "/profile";
                     //  }, 1000);
                     // window.location.href = "/profile";
-                    // window.location.reload();   
-                } else{
+                    // window.location.reload();
+                } else {
                     bootbox.alert({
                         message: data.success,
-                        className: 'rubberBand animated'
+                        className: "rubberBand animated",
                     });
 
-                    setTimeout(function(){
-                            window.location.href = "/profile";
+                    setTimeout(function () {
+                        window.location.href = "/profile";
                     }, 1000);
 
                     // sessionStorage.setItem('token',data.token);
                 }
             },
-            error: function(error) {
+            error: function (error) {
                 console.log(error);
-                bootbox.alert(error)
-
-            }
+                bootbox.alert(error);
+            },
         });
-    });//login
-    
+    }); //login
 
-    $("#logoutbtnSubmit").on('click', function(e) {
+    $("#logoutbtnSubmit").on("click", function (e) {
         e.preventDefault();
 
-        var data = $('#loginForm')[0];
+        var data = $("#loginForm")[0];
         var formData = new FormData(data);
         console.log(data);
 
@@ -69,21 +70,21 @@ $(document).ready(function(){
             data: formData,
             contentType: false,
             processData: false,
-            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+            headers: {
+                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+            },
             dataType: "json",
-            success: function(data) {
+            success: function (data) {
                 console.log(data);
                 // table.row(cRow).data(data).invalidate().draw(false);
                 bootbox.alert(data.success);
-                setTimeout(function(){
+                setTimeout(function () {
                     window.location.href = "/signin";
-                 }, 1000);
-                
+                }, 1000);
             },
-            error: function(error) {
+            error: function (error) {
                 console.log(error);
-            }
+            },
         });
-    });//end update
-
-    })
+    }); //end update
+});
